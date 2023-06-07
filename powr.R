70f1ab309a4e08c1ec114320bf0b376adeecf488
Beth Bowers <57407056+mebowers5@users.noreply.github.com>
2023-06-07 17:14:38 -0400

    Update powr.R

--- powr.R
+++ powr.R
@@ -1,4 +1,4 @@
-pwr <- function(output, sig.level, power, delta) {
+powr <- function(output, sig.level, power, delta) {
   stats::power.t.test(n = NULL, sd = max(na.omit(output$sd)), 
                       sig.level = sig.level, power = power, delta = delta,
                       type = "paired", alternative = "two.sided")
